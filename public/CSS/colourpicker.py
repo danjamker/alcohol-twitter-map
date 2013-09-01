a7f66e0dbcd92782e315114e94d6feaa2ba53bf2
Daniel Kershaw <d.kershaw1@lancaster.ac.uk>
2013-09-01 18:32:39 -0400

    Heat mapp css

--- public/CSS/colourpicker.py
+++ public/CSS/colourpicker.py
@@ -13,42 +13,31 @@ RED = "#FF0000"
 
 def main():
 
-	Masterday = datetime.datetime(2013, 05, 23)
-
-	# g = geo()
-	# postcodes = g.getPostCodes()
-	# postcoderegions = g.getPostCodeRegions()
-	# regons = g.getRegions()
-	# national = ["National-UK"]
-
-	# days = {"Monday":9,
-	# 		"Tuesday":9,
-	# 		"Wednesday":10,
-	# 		"Thursday":16,
-	# 		"Friday":14,
-	# 		"Saturday":29,
-	# 		"Sunday":23}
+	g = geo()
+	postcodes = g.getPostCodes()
+	postcoderegions = g.getPostCodeRegions()
+	regons = g.getRegions()
+	national = ["National-UK"]
+	client = MongoClient('localhost', 27017)
+	db = client.hadoopOuput
+	collection = db['13082013']
+	scores = []
+	for loc in postcoderegions:
+		for tweet in collection.find({"location": loc }):
+			scores.append(float(tweet["score"]))
+	distribution = np.array(scores)
 
 	Masterday = datetime.datetime(2013, 05, 23)
 	for d in range(0,14):
-		print "d"
-		print d
 		t = (Masterday+datetime.timedelta(days=d)).strftime("%Y-%m-%d") 
-		print t
-		iterateoverList(t)
+		#iterateoverList(distribution, t, postcoderegions)
 		for h in range(0, 23):
 			t = (Masterday+datetime.timedelta(days=d, hours = h)).strftime("%Y-%m-%dT%H") 
-			print t
-			iterateoverList(t)
-	#iterateoverList(postcoderegions)
-	#iterateoverList(regons)
-	#iterateoverList(postcodes)
+			iterateoverList(distribution, t, postcoderegions)
 
 	
-
-	
-def iterateoverList(time):
-	
+def iterateoverList(distribution, time, location):
+	print time
 	red = Color("red")
 	blue = Color("blue")
 	colours = list(red.range_to(blue, 100))
@@ -58,17 +47,18 @@ def iterateoverList(time):
 	db = client.hadoopOuput
 	collection = db['13082013']
 
-	scores = []
+
 	with open(time+".css", "a") as myfile:
 		for tweet in collection.find({"time": time }):
-			scores.append(float(tweet["score"]))
-
-		for tweet in collection.find({"time": time }):
 			for i in range(0, 100):
-				if tweet["score"] < scoreatpercentile(np.array(scores), i):
-					print tweet["location"]
-					myfile.write("#"+tweet["location"]+"{fill:"+str(colours[i])+"}\n")
-					break
+				if tweet["type"] == "hourly":
+					if tweet["location"] in location:
+						if tweet["score"] < scoreatpercentile(distribution, i):
+							myfile.write("#"+tweet["location"]+"{fill:"+str(colours[i])+"}\n")
+							break
+
+	print "Exiting"
+	print ""
 
 
 if __name__ == '__main__':
