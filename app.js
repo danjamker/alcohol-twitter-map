8c8257c3d9c12479b28232797595246d67da86ce
Daniel Kershaw <d.kershaw1@lancaster.ac.uk>
2013-09-02 17:06:19 -0400

    Adding tile GEOJSON

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
-
+  , path = require('path')
+  , mongo = require('mongodb');
+  
 var app = express();
 
 // all environments
@@ -27,9 +28,38 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+
+
+var mongoUri =  mongodb://danjamker:apple@ds027668.mongolab.com:27668/geolocations
+
+mongo.Db.connect(mongoUri, function (err, db) {
+ db.collection('UKPostcode').find({ loc :
+                  { $geoWithin :
+                    { $geometry :
+                      { type : "Polygon" ,
+                        coordinates : [ [
+                                          [ 0 , 0 ] ,
+                                          [ 3 , 6 ] ,
+                                          [ 6 , 1 ] ,
+                                          [ 0 , 0 ]
+                                        ] ]
+                } } } });
+});
+
 app.get('/', routes.index);
 
-app.get('/geoJSON/{x}/{y}.json', function (req, res) {
+app.get('/geoJSON/:x/:y/:e/:w/:q.json', function (req, res) {
+    
+    mongo.Db.connect(mongoUri, function (err, db) {
+        var tmp = [[req.params.y,req.params.e],[req.params.w,req.params.q]]
+      db.collection('places').find({loc: {$within: {$box: tmp}}).toArray(function(err, docs) {
+        if(err) return console.dir(err)
+    
+        assert.equal(docs.length, 2);
+      });
+      
+});
+    
     res.sendfile("./GeoJSON/National.json");
 })
 
