b9737d9ffac0b125447ed9bdc354bac972bca188
Daniel Kershaw <danjamker@gmail.com>
2013-09-05 15:09:59 +0100

    More stuff

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , moment = require('moment');
+
 
   var Db = require('mongodb').Db,
     MongoClient = require('mongodb').MongoClient,
@@ -18,7 +20,48 @@ var express = require('express')
     GridStore = require('mongodb').GridStore,
     Grid = require('mongodb').Grid,
     Code = require('mongodb').Code,
-    BSON = require('mongodb').pure().BSON;
+    BSON = require('mongodb').pure().BSON
+  , DataProvider = require('./dataprovider').DataProvider;
+
+var sort = function (list) {
+ 
+    var comparisons = 0,
+        swaps = 0,
+        endIndex = 0,
+        len = list.length - 1,
+        hasSwap = true;
+ 
+    for (var i = 0; i < len; i++) {
+ 
+        hasSwap = false;
+ 
+        for (var j = 0, swapping, endIndex = len - i; j < endIndex; j++) {
+            comparisons++;
+            console.log(list[j])
+            console.log(moment(list[i]).isBefore(list[i + 1]))
+            if (moment(list[j][0]).isAfter(list[j + 1][0])) {
+         
+                swapping = list[j];
+ 
+                list[j] = list[j + 1];
+                list[j + 1] = swapping;
+ 
+                swaps++;
+                hasSwap = true;
+            };
+        };
+ 
+        if (!hasSwap) {
+            break;
+        }
+    }
+ 
+    console.log("--Bubble Sort--")
+    console.log("Comparisons: " + comparisons);
+    console.log("Swaps: " + swaps);
+                 
+    return list;
+};
 
 var app = express();
 
@@ -32,6 +75,7 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+var dataProvider = new DataProvider('localhost', 27017);
 
 // development only
 if ('development' == app.get('env')) {
@@ -132,6 +176,39 @@ app.get('/National.json', function (req, res) {
 })
 app.get('/Regional.json', function (req, res) {
     res.sendfile("./GeoJSON/Regional.json");
+})
+
+app.get('/:postcode', function (req, res) {
+  console.log(req.params.postcode)
+  dataProvider.findAll(req.params.postcode, function(error, emps){
+
+        data = []
+        scores = []
+
+        for (var i = 0; i < emps.length; i++) {
+          element = emps[i];
+          if(element.type == "daily"){
+            console.log(element.time)
+            data.push([element.time,element.score])
+          }
+        }
+
+          list = sort(data)
+
+          tmp = []
+          for(var i = 0; i < list.length; i++){
+            tmp.push(moment(list[i][0]).format('dddd'))
+            scores.push(list[i][1])
+          }
+          // Do something with element i.
+        
+
+        res.render('postcode', {
+              title: req.params.postcode,
+              days: JSON.stringify(tmp),
+              scores: scores
+          });
+    });
 })
 
 http.createServer(app).listen(app.get('port'), function(){
